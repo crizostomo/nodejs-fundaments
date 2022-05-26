012c1c6e0731cbde89677481d65776fc6b6df660
crizostomo <diogo_crizostomo@hotmail.com>
2022-05-26 13:32:21 -0300

    Used Errors, Async, Await and Promises

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
-console.log(chalk.blueBright('vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+function treatError(error){
+  throw new Error(chalk.red(error.code, 'No file found'));
 }
 
-console.log(texto(paragrafo));
+async function pickFile(filePath){
+  const encoding = 'utf-8';
+  try{
+    const text = await fs.promises
+    .readFile(filePath, encoding)
+    console.log(chalk.green(text));
+  } catch(error){
+    treatError(error);
+  }
+}
+
+//function pickFile(filePath){
+//  const encoding = 'utf-8';
+//  fs.promises
+//  .readFile(filePath, encoding)
+//  .then((text) => console.log(chalk.blue(text)))
+//  .catch((error) => treatError(error))
+//}
+
+//function pickFile(filePath){
+//  const encoding = 'utf-8';
+//  fs.readFile(filePath, encoding, (error, text) =>{
+//    if(error){
+//      treatError(error);
+//    }
+//    console.table(chalk.blue(text));
+//  })
+//}
+
+pickFile('./arquivos/texto1.md');
