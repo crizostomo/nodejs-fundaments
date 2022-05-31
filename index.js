8c8e117c69abf3a7c7f8dc71284437a231dc431f
crizostomo <diogo_crizostomo@hotmail.com>
2022-05-30 21:19:03 -0300

    using REGEX

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+const text = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
+
+function extracLink(text){
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const extractedLinks = text.match(regex);
+  console.log(extractedLinks);
+}
+
+extracLink(text);
+
 function treatError(error){
   throw new Error(chalk.red(error.code, 'No file found'));
 }
@@ -34,4 +44,4 @@ async function pickFile(filePath){
 //  })
 //}
 
-pickFile('./arquivos/texto1.md');
+//pickFile('./arquivos/texto1.md');
