0bb6b1aa64ab2d88bd0f67ab5ea7ccc5272ffe81
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-01 05:21:19 -0300

    Code has been refactored

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-const text = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
-
 function extracLink(text){
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-  const extractedLinks = text.match(regex);
-  console.log(extractedLinks);
+const resultsArray = [];
+let temp;
+while((temp = regex.exec(text)) !== null){
+  resultsArray.push({[temp[1]]: temp[2]});
 }
-
-extracLink(text);
+  return resultsArray;
+}
 
 function treatError(error){
   throw new Error(chalk.red(error.code, 'No file found'));
@@ -20,7 +20,7 @@ async function pickFile(filePath){
   try{
     const text = await fs.promises
     .readFile(filePath, encoding)
-    console.log(chalk.green(text));
+    console.log(extracLink(text));
   } catch(error){
     treatError(error);
   }
@@ -44,4 +44,4 @@ async function pickFile(filePath){
 //  })
 //}
 
-//pickFile('./arquivos/texto1.md');
+pickFile('./arquivos/texto1.md');
