56462d1cb9ac78fa1fdfaa1737851491d783c4b2
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-11 16:07:33 -0300

    Validating links

--- http-validation.js
+++ http-validation.js
@@ -1,7 +1,12 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+function generateURLsArray(linksArray){
+//    Object.values(object)
+return linksArray.map(objectLink => Object.values(objectLink).join());
+}
+
 function validateURLs(linksArray) {
-    return linksArray;
+    return generateURLsArray(linksArray);
 }
 
 module.exports = validateURLs;
