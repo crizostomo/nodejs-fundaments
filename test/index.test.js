f7387ec6e88707077efa1a01d02b1556a63b700b
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-11 17:18:18 -0300

    Adding Tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,23 @@
 const pickFile = require('../index');
 
-test('It should be a function', () => {
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('pickFile::', () => {
+    it('iIt should be a function', () => {
+        expect(typeof pickFile).toBe('function');
+    })
+    it('should return array with results', async () => {
+        const result = await pickFile('C:\Users\diogo\OneDrive\Documentos\IT Course\Node\nodejs-fundaments\test\arquivos\texto1.md')
+        expect(result).toEqual(arrayResult)
+    })
+})
+
+/*test('It should be a function', () => {
     expect(typeof pickFile).toBe('function');
-});
+});*/
+
+//It will not pass due to the absolute path
