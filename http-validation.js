836a0ef5b7ab23dad2d190397430480e901bbb85
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-11 16:51:17 -0300

    Building the object

--- http-validation.js
+++ http-validation.js
@@ -1,22 +1,38 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+function errorHandler(err) {
+    throw new Error(err.message);
+}
+
 async function checkStatus(URLsArray){
-    const statusArray = await Promise.all(URLsArray.map(async url =>{
-        const res = await fetch(url)
-        return res.status;
-    }))
-    return statusArray;
+    try {
+        const statusArray = await Promise
+            .all(URLsArray
+                .map(async url =>{
+                const res = await fetch(url)
+                return res.status;
+        }))
+        return statusArray;
+    } catch (err) {
+        errorHandler(err);
+    }
 }
 
 function generateURLsArray(linksArray){
 //    Object.values(object)
-return linksArray.map(objectLink => Object.values(objectLink).join());
+return linksArray
+    .map(objectLink => Object
+        .values(objectLink).join());
 }
 
 async function validateURLs(linksArray) {
     const links = generateURLsArray(linksArray);
     const statusLinks = await checkStatus(links);
-    return statusLinks;
+    
+    const results = linksArray.map((object, index) => ({ ...object, 
+        status: statusLinks[index]
+    }))
+    return results;
 }
 
 module.exports = validateURLs;
